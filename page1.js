b1662a16ee3e450b32e4d5765d4ab1da2342d252
mariem monceur <monceurmariem@gmail.com>
2024-12-25 13:04:12 +0100

    manipuler des tableaux

--- page1.js
+++ page1.js
@@ -1,7 +1,14 @@
-const ticket={
-    nomFilm: "bateman",
-    prix:15,
-    numeroSalle : 10
-}
-var nom ="mariem monceur";
-console.log("Bonjour "+nom+ " votre film : " +ticket.nomFilm +" est en salle "+ticket.numeroSalle )
+// const ticket={
+//     nomFilm: "bateman",
+//     prix:15,
+//     numeroSalle : 10
+// }
+// var nom ="mariem monceur";
+// console.log("Bonjour "+nom+ " votre film : " +ticket.nomFilm +" est en salle "+ticket.numeroSalle )
+
+let playlist=["KEHLANI","Terrance LOve you ","youre on your own kid"];
+var nombre=playlist.length;
+playlist.push("wicked","crazy in love");
+playlist.pop();
+console.log(playlist)
+
