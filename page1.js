96029b8a9951c5b855e4468b20dbcd5b70ce1da1
mariem monceur <monceurmariem@gmail.com>
2024-12-25 11:54:59 +0100

    manipuler des variables

--- page1.js
+++ page1.js
@@ -1,2 +1,7 @@
-let monAge= 42 
-console.log(monAge)
+const ticket={
+    nomFilm: "bateman",
+    prix:15,
+    numeroSalle : 10
+}
+var nom ="mariem monceur";
+console.log("Bonjour "+nom+ " votre film : " +ticket.nomFilm +" est en salle "+ticket.numeroSalle )
